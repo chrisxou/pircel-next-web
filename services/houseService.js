aa600d72b0195e47acc23627def6d1a43d279cf9
chrisxou <chrisxouk@yahoo.gr>
2024-03-04 10:20:22 +0200

    Changed backend url  and used as fallback the API url provided

--- services/houseService.js
+++ services/houseService.js
@@ -1,15 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:8000'; // Replace with your actual API URL
+const BASE_URL_PRIMARY = process.env.NEXT_PUBLIC_NODE_SERVER || "https://pircel-node.onrender.com";
+const BASE_URL_FALLBACK = process.env.NEXT_PUBLIC_PIRCEL_SERVER || "https://wizard-world-api.herokuapp.com";
 
 export const getHouses = async (name) => {
   try {
-    const response = await axios.get(`${BASE_URL}/houses`, {
-      params: {name},
+    const response = await axios.get(`${BASE_URL_PRIMARY}/houses`, {
+      params: { name },
     });
     return response;
   } catch (error) {
-    console.error('There was an error fetching the houses:', error);
-    throw error;
+    console.error("Primary API failed. Trying fallback API...");
+    try {
+      const response = await axios.get(`${BASE_URL_FALLBACK}/houses`, {
+        params: { name },
+      });
+      return response;
+    } catch (error) {
+      console.error("Both APIs failed:", error);
+      throw error;
+    }
   }
 };
